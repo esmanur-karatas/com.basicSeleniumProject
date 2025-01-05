d1777427ee3f2d37adaf531993c4643f1b836ca3
Esmanur <esmanurkaratas0@gmail.com>
2025-01-05 13:31:13 +0300

    Button sayısı yazdırma ve buton isimleri testi

--- src/test/java/BestbuyButtonTest/BestbuyButtonTest.java
+++ src/test/java/BestbuyButtonTest/BestbuyButtonTest.java
@@ -1,12 +1,39 @@
 package BestbuyButtonTest;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+
 public class BestbuyButtonTest {
     public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
         //bestbuy sayfasına gidin
+        driver.get("https://www.bestbuy.com/");
+
         //cookies çıkarsa kabul edin
+        //Çıkmadı
+
         //sayfada kaç adet button bulunduğunu yazdırın
+        List<WebElement> button = driver.findElements(By.tagName("button"));
+        System.out.println("Toplam Buton Sayısı: " + button.size());
+
         //sayfadaki her buttonun üzerindeki yazıları yazdırın.
+        for(WebElement each : button){
+            System.out.println(each.getText());
+        }
+        driver.close();
 
     }
 
 }
+
